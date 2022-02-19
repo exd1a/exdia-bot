fa8b8e5266ae8a8155cba178782d3a1f144a778b
Exdia <exdia8brony@abv.bg>
2022-02-19 02:49:07 +0200

    🤯 Attempted to add Google Docs API Added help message (%help)

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 // const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-// const GoogleDocs = require("googledocs");
-// import {google} from GoogleDocs;
+const { google } = require("googleapis");
 const infos = require("./infos.json");
+const documentId = infos.testDocsID;
+const helpMessage = infos.helpMessage;
 const TOKEN_DISCORD = infos.discordToken;
 // const ChannelID = infos.channelID;
 // const ChannelID2 = infos.channelID2;
+const auth = new google.auth.GoogleAuth({
+	keyFile: "credentials.json",
+	scopes: "https://googleapis.com/auth/documents",
+});
+function basicReplyWithArgs(arr, name, msg, args, command) {
+	arr.forEach(element => {
+		if (command === element) {
+			if (args.length === 0) {
+				msg.reply(`nothing to see here ${name}`);
+				return;
+			}
+			msg.reply(`poopers ${name}: ${args.join(" ")}`);
+		}
+	});
+}
 client.on("ready", function() {
 	console.log("Logged in as ".concat(client.user.tag, "!"));
 });
-client.login(TOKEN_DISCORD);
 const prefix = "%";
+client.login(TOKEN_DISCORD);
 const set1 = infos.set1;
 const set2 = infos.set2;
 const set3 = infos.set3;
-client.on("messageCreate", function(msg) {
+client.on("messageCreate", async function(msg) {
+	const googleClient = await auth.getClient();
+	const googleDocs = google.docs({ version: "v1", auth: googleClient });
+	const metadata = await googleDocs.documents.get({
+		auth,
+		documentId,
+	});
+	console.log(metadata.data);
 	if (msg.author.bot) {return;}
-	// if (msg.channel.id !== ChannelID) {return;}
+	// if (msg.channelId !== ChannelID) {return;}
 	if (!msg.content.includes(prefix)) {return;}
 	const commandBody = msg.content.slice(prefix.length);
 	const args = commandBody.split(" ");
 	const command = args.shift().toLowerCase();
-	set1.forEach(element => {
-		if (command === element) {
-			msg.reply(set1[3]);
-		}
-	});
-	set2.forEach(element => {
-		if (command === element) {
-			msg.reply(set2[6]);
-		}
-	});
-	set3.forEach(element => {
-		if (command === element) {
-			msg.reply(set3[2]);
-		}
-	});
 	if (command === "ping") {
 		msg.reply("pong!");
 	}
@@ -44,4 +52,11 @@ client.on("messageCreate", function(msg) {
 		console.log(args);
 		msg.reply(args.join(" "));
 	}
+	if (command === "help") {
+		console.log(args);
+		msg.reply(helpMessage);
+	}
+	basicReplyWithArgs(set1, "user1", msg, args, command);
+	basicReplyWithArgs(set2, "user2", msg, args, command);
+	basicReplyWithArgs(set3, "user3", msg, args, command);
 });
